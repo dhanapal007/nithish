80aadac642c37e1cea41e864288e12c769cd6e0e
dhanapal007 <94822368+dhanapal007@users.noreply.github.com>
2021-11-22 11:34:53 +0530

    Add files via upload

---
+++ 10 marks calc.py
@@ -0,0 +1,11 @@
+#mark calculation
+name=(input("enter the student name :"))
+tamil = int(input("enter the tamil mark :"))
+english = int(input("enter the eng mark :"))
+maths=int(input("enter the math mark :"))
+science =int(input("enter the science mark :"))
+social=int(input("enter the social mark :"))
+total=tamil+english+maths+science+social
+average=total/5
+print("Total marks :",total)
+print("average  :",average)
